e052aeb87de5363722a05a2a66d306912ab0a05d
goergemiguel <goergemiguel21@gmail.com>
2023-05-13 02:56:48 +0800

    feat: use custom tailwind css theme for formkit

--- formkit.config.js
+++ formkit.config.js
@@ -1,12 +1,48 @@
 import { plugin, defaultConfig } from '@formkit/vue'
 import { createProPlugin, inputs } from '@formkit/pro'
+import { generateClasses } from '@formkit/themes'
 
-import '@formkit/themes/genesis'
-import '@formkit/pro/genesis'
+import {
+    arrowDown,
+    arrowUp,
+    check,
+    circle,
+    close,
+    down,
+    fileDoc,
+    spinner,
+    star,
+    trash,
+    date,
+    left,
+    right,
+} from '@formkit/icons'
+import theme from './formkit-theme.js'
 
 const proPlugins = createProPlugin('fk-2ed9ffbc9', inputs)
 const config = defaultConfig({
     plugins: [proPlugins],
+    config: {
+        classes: generateClasses(theme),
+    },
+    icons: {
+        // include supporting icons from @formkit/icons
+        arrowDown,
+        arrowUp,
+        close,
+        checkboxDecorator: check,
+        fileItem: fileDoc,
+        fileRemove: close,
+        noFiles: fileDoc,
+        radioDecorator: circle,
+        select: down,
+        spinner,
+        star,
+        trash,
+        date,
+        left,
+        right,
+    },
 })
 
 export default config
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const formKitTailwind = require('@formkit/themes/tailwindcss')
+
 export default {
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    content: [
+        './index.html',
+        './src/**/*.{vue,js,ts,jsx,tsx}',
+        './formkit.config.js',
+        './formkit-theme.js',
+    ],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [formKitTailwind],
 }
