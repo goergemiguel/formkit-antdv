7109722b4264d677a636c5523a7a88ec02048106
goergemiguel <goergemiguel21@gmail.com>
2023-05-15 12:56:46 +0800

    fix: hide label on button

--- src/formkit/library/button.ts
+++ src/formkit/library/button.ts
@@ -4,24 +4,27 @@ import { withDefaultFormKitSchema } from './utils'
 export default {
     type: 'input',
     props: ['label', 'antProps'],
-    schema: withDefaultFormKitSchema({
-        if: '$antProps',
-        then: [
-            {
-                $cmp: AntButton,
-                children: ['$label'],
-                bind: '$antProps',
-            },
-        ],
-        else: [
-            {
-                $cmp: AntButton,
-                children: ['$label'],
-                bind: '$antProps',
-                props: {
-                    type: 'default',
+    schema: withDefaultFormKitSchema(
+        {
+            if: '$antProps',
+            then: [
+                {
+                    $cmp: AntButton,
+                    children: ['$label'],
+                    bind: '$antProps',
                 },
-            },
-        ],
-    }),
+            ],
+            else: [
+                {
+                    $cmp: AntButton,
+                    children: ['$label'],
+                    bind: '$antProps',
+                    props: {
+                        type: 'default',
+                    },
+                },
+            ],
+        },
+        { showLabel: false }
+    ),
 }
